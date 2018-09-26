7bd523df119c067cec673f052b32607360acba4b
Casper Steinmann <casper.steinmann@gmail.com>
2018-09-26 22:46:01 +0200

    first batch of hf-3c energies

---
+++ 1-2/results/extract_hf-3c_energies.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+
+import csv
+import os
+import sys
+
+import numpy
+
+
+def read_xyz_energy(filename):
+    with open(filename, 'r') as fhf:
+        for line2 in fhf:
+            if "FINAL SINGLE POINT" in line2:
+                tokens2 = line2.split()
+                energy = float(tokens2[-1])*627.51
+                return energy
+
+
+def iter_files(system, index):
+    """ iterator for files in project """
+    for i in range(1, 4001):
+
+        p = "../{0:d}00/{1:s}/V{0:d}".format(index, system)
+        pp = "../000/{0:s}/V0".format(system)
+        cplx = os.path.join(p, "c{0:d}_hf.out".format(i))
+        recp = os.path.join(pp, "r{0:d}_hf.out".format(i))
+        lig = os.path.join(p, "l{0:d}_hf.out".format(i))
+        yield i, cplx, recp, lig
+
+
+def iter_energies(system, index):
+    for i, c, r, l in iter_files(system, index):
+        c_erg = read_xyz_energy(c)
+        r_erg = read_xyz_energy(r)
+        l_erg = read_xyz_energy(l)
+        yield i, c_erg, r_erg, l_erg, c_erg - r_erg - l_erg
+
+
+if __name__ == '__main__':
+    data = []
+    for system in ['prot', 'wat']:
+        for index in [0]: #[0, 1]
+            filename = "{1:d}00_{0:s}_hf-3c_energies.csv".format(system, index)
+            with open(filename, 'w') as csvfile:
+                writer = csv.writer(csvfile)
+                for i, c, r, l, de in iter_energies(system, index):
+                    writer.writerow([i, c, r, l, de])
